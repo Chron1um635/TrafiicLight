ef4fb69195f0c1aca2c18cc3076f3d003817f9bf
Chron1um635 <chron1um522@gmail.com>
2024-10-06 18:39:54 +0300

    Initial Commit

---
+++ TrafiicLight/TrafiicLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafiicLightApp.swift
+//  TrafiicLight
+//
+//  Created by Максим Назаров on 06.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct TrafiicLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
